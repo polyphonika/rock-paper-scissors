3cde58f1e35492e8e084f1bce490265baf79e9f9
Ryan Berry <email@cornerhousemedia.com>
2024-09-21 15:44:46 +0100

    all logic done, all works

--- script.js
+++ script.js
@@ -104,46 +104,82 @@ function playGame(rounds, humanScore, computerScore, btnChoice) {
 }
 
 function btnPress(e) {
-        // console.log(e.target.textContent);
-        //play game, get returned values
-        let {computerChoice, winner} = playRound(e.target.textContent, getComputerChoice());
-        roundContent.textContent = roundCount;
-        
-        if (!playerHand) {
-                        
-            playerHand = document.createElement('p');
-            playerHand.classList.add('player-hand');
-            playerHand.textContent = `Player chose: ${e.target.textContent}`;
-            document.querySelector('.played-hands').appendChild(playerHand);
+    let maxWins = 5;
 
-            computerHand = document.createElement('p');
-            computerHand.classList.add('computer-hand');
-            computerHand.textContent = `Computer chose: ${computerChoice}`;
-            document.querySelector('.played-hands').appendChild(computerHand);
-
-            winningHand = document.createElement('p');
-            winningHand.classList.add('winning-hand');
-            winningHand.textContent = `Winner: ${winner}`;
-            document.querySelector('.played-hands').appendChild(winningHand);
-        }
-
+    let {computerChoice, winner} = playRound(e.target.textContent, getComputerChoice());
+    roundContent.textContent = roundCount;
+    
+    if (!playerHand) {
+                    
+        playerHand = document.createElement('p');
+        playerHand.classList.add('player-hand');
         playerHand.textContent = `Player chose: ${e.target.textContent}`;
+        document.querySelector('.played-hands').appendChild(playerHand);
+
+        computerHand = document.createElement('p');
+        computerHand.classList.add('computer-hand');
         computerHand.textContent = `Computer chose: ${computerChoice}`;
+        document.querySelector('.played-hands').appendChild(computerHand);
+
+        winningHand = document.createElement('p');
+        winningHand.classList.add('winning-hand');
         winningHand.textContent = `Winner: ${winner}`;
+        document.querySelector('.played-hands').appendChild(winningHand);
+    }
+
+    playerHand.textContent = `Player chose: ${e.target.textContent}`;
+    computerHand.textContent = `Computer chose: ${computerChoice}`;
+    winningHand.textContent = `Winner: ${winner}`;
+    
+    if (winner === 'Player') {
+        playerScoreDisplay.textContent = ++playerScore;
+        computerScoreDisplay.textContent = computerScore;
+    } else if (winner === 'Computer') {
+        playerScoreDisplay.textContent = playerScore;
+        computerScoreDisplay.textContent = ++computerScore;
+    } else {
+        playerScoreDisplay.textContent = playerScore;
+        computerScoreDisplay.textContent = computerScore;
+        tieScoreDisplay.textContent = ++tieScore;
+    }
+    
+    if (playerScore==maxWins || computerScore==maxWins) {        
+        endGame(winner);
+    }
+
+    roundCount++;
+}
+
+function endGame(winner) {    
+    // console.log('game over, winner declared');
+    buttons.forEach(button => {
+        console.log(button);
+        button.removeEventListener('click', btnPress);
+        button.style.cursor = 'not-allowed';
+        button.style.opacity = 0.5;
+        button.style.pointerEvents = 'none';
+    });
+
+    let scoreHeader = document.querySelector('h2.score-header');
+    scoreHeader.textContent = `Game over, ${winner} wins is the winner!`;
+
+    let btnPlayAgain = document.createElement('button');
+    btnPlayAgain.textContent = 'Play again?';
+    const header = document.querySelector('header');
+    header.insertAdjacentElement('afterend',btnPlayAgain);
+    btnPlayAgain.addEventListener('click', () => {
+        location.reload();
+    })
+    
+    
+}
+
+function playGameV2() {
+    buttons.forEach(button => {
+        // const playerHand = document.querySelector('p.player-hand');
+        button.addEventListener('click', btnPress);
         
-        if (winner === 'Player') {
-            playerScoreDisplay.textContent = ++playerScore;
-            computerScoreDisplay.textContent = computerScore;
-        } else if (winner === 'Computer') {
-            playerScoreDisplay.textContent = playerScore;
-            computerScoreDisplay.textContent = ++computerScore;
-        } else {
-            playerScoreDisplay = playerScore;
-            computerScoreDisplay = computerScore;
-            tieScoreDisplay.textContent = ++tieScore;
-        }
-        
-        roundCount++;
+    });
 }
 
 // set up event listeners
@@ -154,15 +190,15 @@ let computerHand = document.querySelector('p.computer-hand');
 let playerScoreDisplay = document.querySelector('p.player-score span');
 let computerScoreDisplay = document.querySelector('p.computer-score span');
 let tieScoreDisplay = document.querySelector('p.tie-score span');
+
 let playerScore = 0;
 let computerScore = 0;
 let tieScore = 0;
 let roundCount = 1;
 
-buttons.forEach(button => {
-    // const playerHand = document.querySelector('p.player-hand');
-    button.addEventListener('click', btnPress)
-});
+playGameV2();
+
+
 
 // playGame(5, 0, 0);
 
